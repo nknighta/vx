34999feae08f368565ece00cb8932cceb25fcb68
nknighta <n.knight.pc0627@gmail.com>
2023-12-13 16:44:14 +0900

    feat: redirect server

--- layout/main.tsx
+++ layout/main.tsx
@@ -22,38 +22,7 @@ const Layout = ({ children }) => {
                     <VARIUSHeader />
                     {children}
                 </WagmiConfig>
-                        <footer style={{
-                            background: "#000000",
-                        }}>
-                            <div style={{
-                                margin: "0 auto",
-                                padding: "8.2vh 30px 10vh 30px",
-                                display: "flex",
-                                justifyContent: "center",
-                            }}>
-                                <Box>
-                                    <Box>
-                                        Nknight AMAMIYA © 2023 - ver:{pack.version}
-                                    </Box>
-                                    <Link href={"https://github.com/nknighta/"}>
-                                        <Text p={2}>
-                                            GitHub
-                                        </Text>
-                                    </Link>
-                                    <Link href={"https://twitter.com/ama_dev_1/"}>
-                                        <Text p={2}>
-                                            Twitter
-                                        </Text>
-                                    </Link>
-                                    <Link href={"https://youtube.com/@ama_p213/"}>
-                                        <Text p={2}>
-                                            Youtube
-                                        </Text>
-                                    </Link>
-                                </Box>
-                            </div>
-                        </footer>
-                    </ChakraProvider>
+            </ChakraProvider>
         </div>
     );
 };
