03ecf8aa295d82e0303bc17e98e4d2044a31043e
n.knight.pc0627@gmail.com <n.knight.pc0627@gmail.com>
2024-09-23 23:56:55 +0900

    feat ✨ add internal api
    get useedata system using prisma

--- src-server/idata/github.ts
+++ src-server/idata/github.ts
@@ -1,8 +1,15 @@
 import Express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const x9gitapi = Express();
 
 import bodyParser from "body-parser";
+
+// Middleware to parse JSON bodies
+// Middleware to parse URL-encoded bodies
+
+// instantiate the prisma client
+const prisma = new PrismaClient();
 
 x9gitapi.use(bodyParser.urlencoded({
     extended: true
@@ -14,9 +21,18 @@ x9gitapi.get('/vx-inter-api/datainfo/', (req, res) => {
     res.status(200).send({ msg: "'GET request received'" });
 });
 
-x9gitapi.post('/vx-inter-api/userdata/search/github/', (req, res) => {
-  console.log(req.body);
-  res.send({"msg": "Received POST Data!","data": req.body});
+// must await prisma.user.findMany() to get the data from the database
+x9gitapi.post('/vx-inter-api/userdata/search/github/', async ( req, res) => {
+  try {
+    const data = await prisma.account.findUnique({
+        where: {
+          accountid: req.body.userid,
+        },
+      });
+      res.send({"data": data});
+  } catch (error) {
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 });
 
 export default x9gitapi;
