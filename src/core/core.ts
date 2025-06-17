2936b091823826f1bb047d814a056dd800f3ef05
nknighta <n.knight.pc0627@gmail.com>
2025-06-17 17:43:28 +0900

    update: 👆 new config method

--- src/core/core.ts
+++ src/core/core.ts
@@ -1,24 +1,13 @@
-interface Config {
-  host: string;
-  port: number;
-  sslpath?: string;
-}
+export class createConfig {
+  name: string;
+  nodeconfigbuilder: any[];
 
-export function vx(a?: string) {
-  console.log('hello!' + a);
-}
-
-export function createConfig({ host, port, sslpath }: Config) {
-  if (!host || !port) {
-    console.error('Host and port are required parameters.');
-    process.exit(1);
-  } else if (typeof host !== 'string' || typeof port !== 'number') {
-    console.error('Host and port are required parameters.');
-    process.exit(1);
+  constructor(name: string, nodeconfigbuilder?: any[]) {
+    this.name = name;
+    this.nodeconfigbuilder = nodeconfigbuilder || [];
   }
-  return {
-    host: host,
-    port: port,
-    sslpath: sslpath || undefined,
-  } as Config;
+
+  userNodeConfigulation() {
+    console.log(`Creating contract on host: ${this.name} with config: ${this.nodeconfigbuilder}`);
+  }
 }
